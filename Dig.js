ec2fa6964214e6f71a1ad633c62cd858c28d9f76
William Johnston <wjoh0002@student.monash.edu>
2020-03-19 11:55:31 +1100

    added table scraping function, scrapeTables

--- Dig.js
+++ Dig.js
@@ -24,67 +24,121 @@ class Dig {
     applyProperties(this, options);
   }
 
-  // call before you do anything
+  // Call before you do anything
   async open() {
+
     this.browser = await puppeteer.launch({
       headless: this.headless,
       sandbox: this.sandbox,
       args: this.args
     });
+
     this.page = await this.browser.newPage();
     await this.page.setUserAgent(this.userAgent);
     console.log("Browser and page are open");
+
   }
 
-  // call after you're done doing things
+  // Call after you're done doing things
   async close() {
+
     await this.page.close();
     await this.browser.close();
     console.log("Browser and page are closed");
+    
   }
 
-  async digUp(url, instructions) {
-    await this.page.goto(url);
-    var thisHandle = this.page;
-    for (var instruction of instructions) {
-      var waitFor = instruction.waitFor != undefined ? instruction.waitFor : instruction.selector;
-      await this.page.waitForSelector(waitFor);
-      const selections = await thisHandle.$$(instruction.selector);
-      if (instruction.index != undefined) {
-        thisHandle = selections[instruction.index];
-      } else {
-        var found = false;
-        if (instruction.booleanCallback !== undefined) {
-          for (var selection of selections) {
-            const result = await instruction.booleanCallback(selection);
-            if (result === true) {
-              thisHandle = selection;
-              found = true;
-              break;
-            }
-          }
-        } else if (instruction.indexCallback !== undefined) {
-          var i = 0;
-          for (var selection of selections) {
-            const index = await instruction.indexCallback(selection, i);
-            if (index >= 0) {
-              thisHandle = selections[index]; //select index returned from callback
-              found = true;
-              break;
-            }
-            i++;
-          }
+
+  async collectTableData(rowHandles, columnData, { filterFunction, inFunction, inFunctionParams }){
+    var tableData = [];
+    // Loop through each row handle, extract each cell
+    for (var rowHandle of rowHandles) {
+      var rowObj = {};
+      for ( var {columnName, selector, selectAttribute} of columnData) {
+        try{
+          // By default we grab the innerText attribute, but if we've been given an alternative, select that
+          selectAttribute = selectAttribute || "innerText";
+          var cellValue = await rowHandle.$eval(selector, (e, selectAttribute) => e[selectAttribute], selectAttribute);
         }
-        if (!found) {
-          throw new Error(
-            "failed to find anything from instruction: " +
-              JSON.stringify(instruction)
-          );
+        catch (e) {
+          console.log(`Failed to retrieve selector '${selector}', setting this cell to null.\nError message: ${e.message}\n`);
+          var cellValue = null;
+        }
+        finally {
+          rowObj[columnName] = cellValue;
         }
       }
+      // Perform the inFunction, if any
+      if(inFunction !== undefined){
+        await inFunction(rowObj, rowHandle, inFunctionParams); // Extra parameters can be passed to inFunction, eg the page, browser
+      }
+      // Before we append this row to the output, pass it through the filter function, if any
+      if(filterFunction === undefined || await filterFunction(rowObj)){
+        tableData.push(rowObj);
+        console.log("pushed:", rowObj);
+      }
     }
-    return thisHandle;
+    return tableData;
   }
+
+  
+  async scrapeTables(url, instructions){
+  
+    let { tableSelector, rowSelector, columnData, nextPageSelector, preFunction, inFunction, filterFunction } = instructions;
+  
+    await this.open();
+    let browser = this.browser;
+    let page = this.page;
+  
+    await page.goto(url);
+  
+    // Perform some prefunction, if given
+    if(preFunction != undefined){
+      await preFunction(page, browser);
+    }
+    
+    let tableData = [];
+    let nextPage = true;
+    let pageNum = 1;
+  
+    // Loop through the web pages and collect the table data
+    while(nextPage){
+      // Select the table container, if there is one given, otherwise the page is the top level handle
+      let tableHandle = tableSelector !== undefined ? await page.$(tableSelector) : page;
+  
+      // Select all of the rows on this page
+      let rowHandles = await tableHandle.$$(rowSelector);
+      let inFunctionParams = { page, browser };
+      let thisPageTableData = await collectTableData(rowHandles, columnData, { filterFunction, inFunction, inFunctionParams });
+      tableData = [...tableData, ...thisPageTableData];
+  
+      // If we've been told to scroll through multiple pages,
+      if (nextPageSelector !== undefined){
+        // Then move to the next page - if there is one
+        try{
+          let nextPageHandle = await page.$(nextPageSelector);
+          await nextPageHandle.click();
+          pageNum++;
+          await delay(3000, 3000);
+          await page.waitForSelector(`${tableSelector} ${rowSelector}`); // Ensure our page has loaded
+        }
+        catch (e){
+          console.log(`Caught error trying to navigate to next page, number ${pageNum} - assuming we are done. Error message: ${e.message}`);
+          nextPage = false;
+        }
+      }
+      else {
+        nextPage = false; // If next page is undefined, that's the end anyway
+        console.log(`No next page selector given - finishing up on ${tableData.length} rows.`);
+      }
+  
+    }
+  
+    await this.close();
+    return tableData;
+  
+  }
+
 }
 
 module.exports = Dig;
